21e90f2288114be36261db1051a892bd31f19045
RRuma <47134034+RRuma@users.noreply.github.com>
2019-02-19 13:18:39 +0600

    Add files via upload

---
+++ restapp/urls.py
@@ -0,0 +1,39 @@
+"""restapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls import url
+
+
+api_url = [
+   # path('article/', include('article.urls')),
+
+
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('article.urls')),
+    #path('api/',include(api_url)),
+    path('blog/', include('blog.urls')),
+    #path('cat/', include('category.urls')),
+    path('pro/', include('product.urls')),
+    path('brand/', include('brand.urls')),
+
+    #url(r'^about/$',include('brand.urls')),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^cat/', include('category.urls')),
+]
